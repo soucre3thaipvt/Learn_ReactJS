8ac629453060c4d3ef0769170a520bf139530c0f
thaipvt <thaipvt@d2t.vn>
2022-06-09 10:57:13 +0700

    Install thư viện Router điều hướng || init Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import HomePage from './pages/HomePages'
+import PersonPage from './pages/PersonPages'
+import ProductPage from './pages/ProductPages'
+import SettingPage from './pages/SettingPages'
+import ChatPage from './pages/ChatPages'
+import LoginPage from './pages/LoginPages'
+import SignUpPage from './pages/SignUp'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/person" element={<PersonPage />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/setting" element={<SettingPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
