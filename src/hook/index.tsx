7b4cad8ab587e2f893416e3534fa72ccf68359e3
ThaiPVT <soucre3thaipvt@gmail.com>
2022-06-10 17:42:58 +0700

    Init Hook, Hiểu thế nào là hook, tại sao sử dụng hook

---
+++ src/hook/index.tsx
@@ -0,0 +1,27 @@
+import UseState from "./UseState";
+import UseRef from "./UseRef";
+import UseReducer from "./UseReducer";
+import UseCallback from "./UseCallback";
+import UseEffect from "./UseEffect";
+import UseContext from "./UseContext";
+import UseMemo from './UseMemo';
+import UseLayoutEffect from "./UseLayoutEffect";
+import UseImperativveHandle from "./UseImperativveHandle";
+import UseDebugValue from "./UseDebugValue";
+
+export default {
+    UseState, 
+    UseEffect,
+    UseLayoutEffect,
+    UseRef,
+    UseCallback,
+    UseContext,
+    UseMemo,
+    UseReducer,
+    UseImperativveHandle,
+    UseDebugValue
+ };
+
+ // Class component dùng để xây dựng đầy đủ tính năng xịn xò
+ // Function componet chữa đoạn code jsx để hiển thị component, ko đầy đủ tính năng ( hook ra đời giúp tính năng đầy đủ và xịn xò hơn )
+ 
